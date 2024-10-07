d5242e13cde9b6c281e025ccc41055dd497a683c
Dmytro Zykov <dmytro.zykov@apriorit.com>
2024-10-07 13:33:36 +0300

    Remove noexcept for clang compiler

--- include/sol/function_types_stateless.hpp
+++ include/sol/function_types_stateless.hpp
@@ -320,7 +320,12 @@ namespace sol { namespace function_detail {
 		}
 
 		template <bool is_yielding, bool no_trampoline>
-		static int call(lua_State* L) noexcept(std::is_nothrow_copy_assignable_v<T>) {
+		static int call(lua_State* L) 
+#if SOL_IS_ON(SOL_COMPILER_CLANG)
+#else
+    noexcept(std::is_nothrow_copy_assignable_v<T>)
+#endif
+    {
 			int nr;
 			if constexpr (no_trampoline) {
 				nr = real_call(L);
@@ -360,7 +365,12 @@ namespace sol { namespace function_detail {
 		}
 
 		template <bool is_yielding, bool no_trampoline>
-		static int call(lua_State* L) noexcept(std::is_nothrow_copy_assignable_v<T>) {
+		static int call(lua_State* L)
+#if SOL_IS_ON(SOL_COMPILER_CLANG)
+#else
+    noexcept(std::is_nothrow_copy_assignable_v<T>)
+#endif
+    {
 			int nr;
 			if constexpr (no_trampoline) {
 				nr = real_call(L);
